a7c8938c048f6a12a036f0aa68570eeb5703f1ef
Christian de Botton <cdebotton@gmail.com>
2015-05-23 23:54:57 -0400

    remove existing tests for react middleware.

--- api/middleware/__tests__/reactTest.js
+++ api/middleware/__tests__/reactTest.js
@@ -11,6 +11,21 @@ import { Route } from "react-router";
 chai.should();
 chai.use(sinonChai);
 
+class HTMLDocument extends Component {
+  render() {
+    return (
+      <html lang="en">
+      <head>
+        <title>HTML Document</title>
+      </head>
+      <body>
+        <div dangerouslySetInnerHTML={{ __html: this.props.markup }} />
+      </body>
+      </html>
+    );
+  }
+}
+
 class HandlerComponent extends Component {
   render() {
     return (
@@ -22,30 +37,5 @@ class HandlerComponent extends Component {
 const routes = (<Route handler={ HandlerComponent } />);
 
 describe("react", () => {
-  beforeEach(() => {
-    global.react = require("../react").default;
-    global.resolveRoutes = require("../react").resolveRoutes;
-  });
 
-  afterEach(() => {
-    delete global.react;
-  });
-
-  describe("#resolveRoutes()", () => {
-    it("should resolve a handler and state.", done => {
-      let app = koa();
-
-      app.react = { routes };
-      app.use(react());
-
-      let resolveRoutes = spy(resolveRoutes);
-
-      request(app.listen())
-        .get("/")
-        .end((err, res) => {
-          console.log(resolveRoutes.should.have.been.calledOnce);
-          done();
-        });
-    });
-  });
 });
