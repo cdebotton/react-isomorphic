c219e3d172a3ba9afd7852a754daa493542e77cd
Christian de Botton <cdebotton@gmail.com>
2015-05-27 08:28:50 -0400

    Remove middlewares.

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@
 
 import compress from "koa-compress";
 import session from "koa-session";
+import react from "./middleware/react";
+import alt from "../app/alt";
+import HTMLDocument from "../app/components/HTMLDocument";
 import {
   appKey,
   appSecret,
@@ -14,12 +17,12 @@ app.keys = [appKey, appSecret];
 
 app.use(session(app));
 app.use(compress());
-app.use(function *(next) {
-  this.body = "Test";
-});
+app.use(react({ alt, HTMLDocument }));
 
 const server = require("http").createServer(app.callback());
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+export default app;
